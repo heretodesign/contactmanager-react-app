af909db02711cb729738a2ea5fa39a14a54943e9
heretodesign <senwelo.works@gmail.com>
2019-12-29 14:42:44 +0800

    Added handleSubmit function to handle post req

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
+import { Consumer } from '../../context'
+import uuid from 'uuid'
 
 class AddContact extends Component {
     state = {
         name: '',
         email: '',
-        phone: '',
+        phone: ''
     };
 
-    handleSubmit = (e) => {
+    handleSubmit = (dispatch, e) => {
         e.preventDefault();
         console.log(this.state)
-    }
+        const { name, email, phone } = this.state;
+
+        const newContact = {
+            id: uuid(),
+            name,
+            email,
+            phone
+        }
+
+        dispatch({ type: 'ADD_CONTACT', payload: newContact });
+
+        // Clear State
+        this.setState({
+            name: '',
+            email: '',
+            phone: ''
+        });
+    };
 
     handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
@@ -18,53 +37,60 @@ class AddContact extends Component {
         const { name, email, phone } = this.state;
 
         return (
-            <div className="card mb-3">
-                <div className="card-header">
-                    Add Contact
-                </div>
-                <div className="card-body">
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="">Name</label>
-                            <input 
-                                type="text" 
-                                name="name" 
-                                className="form-control form-control-lg" 
-                                placeholder="Enter Name..." 
-                                value={name}
-                                onChange={this.handleChange}
-                            />
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div className="card mb-3">
+                            <div className="card-header">
+                                Add Contact
+                            </div>
+                            <div className="card-body">
+                                <form onSubmit={this.handleSubmit.bind(this, dispatch)}>
+                                    <div className="form-group">
+                                        <label htmlFor="">Name</label>
+                                        <input 
+                                            type="text" 
+                                            name="name" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Enter Name..." 
+                                            value={name}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input 
+                                            type="email" 
+                                            name="email" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Enter Email..." 
+                                            value={email}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="phone">Phone</label>
+                                        <input 
+                                            type="text" 
+                                            name="phone" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Enter Phone..." 
+                                            value={phone}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                    <input 
+                                        type="submit"
+                                        value="Add Contact"
+                                        className="btn btn-primary btn-block"
+                                    />
+                                </form>
+                            </div>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input 
-                                type="email" 
-                                name="email" 
-                                className="form-control form-control-lg" 
-                                placeholder="Enter Email..." 
-                                value={email}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phone">Phone</label>
-                            <input 
-                                type="text" 
-                                name="phone" 
-                                className="form-control form-control-lg" 
-                                placeholder="Enter Phone..." 
-                                value={phone}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <input 
-                            type="submit"
-                            value="Add Contact"
-                            className="btn btn-primary btn-block"
-                        />
-                    </form>
-                </div>
-            </div>
+                    )
+                }}
+            </Consumer>
         )
     }
 }
