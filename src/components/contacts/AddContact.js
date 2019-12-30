ecfdb1bef71b024dedcfc58a376e4661b413b558
heretodesign <senwelo.works@gmail.com>
2019-12-30 15:03:26 +0800

    added classnames library to handle the error for our form inputs

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,13 +7,33 @@ class AddContact extends Component {
     state = {
         name: '',
         email: '',
-        phone: ''
+        phone: '',
+        errors: {}
     };
 
     handleSubmit = (dispatch, e) => {
         e.preventDefault();
-        console.log(this.state)
         const { name, email, phone } = this.state;
+
+        //Check for Errors
+        if (name === '') {
+            this.setState({
+                errors: { name: 'Name is required' }
+            });
+            return;
+        }
+        if (email === '') {
+            this.setState({
+                errors: { email: 'Email is required' }
+            });
+            return;
+        }
+        if (phone === '') {
+            this.setState({
+                errors: { phone: 'Phone is required' }
+            });
+            return;
+        }
 
         const newContact = {
             id: uuid(),
@@ -28,14 +48,15 @@ class AddContact extends Component {
         this.setState({
             name: '',
             email: '',
-            phone: ''
+            phone: '',
+            errors: {}
         });
     };
 
     handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
     render() {
-        const { name, email, phone } = this.state;
+        const { name, email, phone, errors } = this.state;
 
         return (
             <Consumer>
@@ -54,6 +75,7 @@ class AddContact extends Component {
                                         placeholder="Enter Name"
                                         value={name}
                                         onChange={this.handleChange}
+                                        error={errors.name}
                                     />
                                     <TextInputGroup
                                         label="Email"
@@ -62,6 +84,7 @@ class AddContact extends Component {
                                         placeholder="Enter Email"
                                         value={email}
                                         onChange={this.handleChange}
+                                        error={errors.email}
                                     />
                                     <TextInputGroup
                                         label="Phone"
@@ -69,6 +92,7 @@ class AddContact extends Component {
                                         placeholder="Enter Phone"
                                         value={phone}
                                         onChange={this.handleChange}
+                                        error={errors.phone}
                                     />
 
                                     <input 
--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
-const TextInputGroup = ({ label, name, value, placeholder, type, onChange }) => {
+const TextInputGroup = ({ label, name, value, placeholder, type, onChange, error }) => {
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <input 
                 type={type} 
                 name={name} 
-                className="form-control form-control-lg" 
+                className={classnames("form-control form-control-lg", {
+                    "is-invalid": error 
+                })}
                 placeholder={placeholder} 
                 value={value}
                 onChange={onChange}
             />
+            {error && <div className="invalid-feedback">{error}</div>}
         </div>
     )
 }
@@ -23,7 +27,8 @@ TextInputGroup.propTypes = {
     placeholder: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    error: PropTypes.string
 };
 
 TextInputGroup.defaultProps = {
