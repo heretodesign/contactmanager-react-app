56a2e78ef4755df1a13239c0cbd4f17fa4ba8e6b
heretodesign <senwelo.works@gmail.com>
2019-12-30 15:06:48 +0800

    added react-router-dom library to handle spliting our components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Contacts from './components/contacts/Contacts'
 import Header from './components/layout/Header'
 import AddContact from './components/contacts/AddContact'
