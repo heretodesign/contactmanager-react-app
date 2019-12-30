e5140b7197985b4b94eb4c3916c88b09d394482d
heretodesign <senwelo.works@gmail.com>
2019-12-30 15:21:23 +0800

    split the pages using react-router-dom and added About page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Contacts from './components/contacts/Contacts'
 import Header from './components/layout/Header'
+import About from './components/pages/About'
 import AddContact from './components/contacts/AddContact'
 import { Provider } from './context'
-
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,13 +12,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <Provider>
-      <div className="App">
-        <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+      <Router>
+        <div className="App">
+          <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact" component={Contacts} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     </Provider>
    
   );
