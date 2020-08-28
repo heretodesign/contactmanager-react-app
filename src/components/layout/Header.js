b6b01a754b9a2924d86fa8d35572e1bc2119a1e0
heretodesign <senwelo.works@gmail.com>
2020-08-28 12:58:53 +0800

    header changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const Header = props => {
     const { branding } = props;
@@ -11,10 +12,20 @@ const Header = props => {
                 </a>
                 <div>
                     <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item">
-                            <a href="/" className="navbar-link">
-                                Home
-                            </a>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                               <i className="fas fa-home"></i> Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact/add" className="nav-link">
+                                <i className="fas fa-plus"></i> Add
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/about" className="nav-link">
+                                <i className="fas fa-question"></i>  About
+                            </Link>
                         </li>
                     </ul>
                 </div>
